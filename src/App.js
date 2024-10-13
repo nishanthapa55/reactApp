09180c83eea680cd4958509f4078d0ebb516d591
nishanthapa55 <t3thni00@students.oamk.fi>
2024-10-13 20:26:19 +0300

    Update App.js with custom text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nishan's reactApp
         </p>
         <a
           className="App-link"
